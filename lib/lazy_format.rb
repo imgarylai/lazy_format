f34a5023c1c7c8aea8f048c7ed6aa23871a448be
zxlai <garylai1990@gmail.com>
2015-03-26 10:41:59 +0800

    Register rails engine

--- lib/lazy_format.rb
+++ lib/lazy_format.rb
@@ -1,5 +1,27 @@
 require "lazy_format/version"
 
 module LazyFormat
-  # Your code goes here...
+  class << self
+    def load!
+      register_rails_engine
+    end
+
+    # Paths
+    def gem_path
+      @gem_path ||= File.expand_path('..', File.dirname(__FILE__))
+    end
+
+    # Environment detection helpers
+    def rails?
+      defined?(::Rails)
+    end
+
+    private
+
+    def register_rails_engine
+      require 'lazy_format/rails/railtie'
+    end
+  end
 end
+
+LazyFormat.load!
